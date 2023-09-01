435d293bbf6369b16b77e5bebd46e9dcf3ea780e
samlee088 <108655429+samlee088@users.noreply.github.com>
2023-08-31 20:12:11 -0700

    return token with login

--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,10 +1,31 @@
 import { StyleSheet, Text, View } from "react-native";
+import axios from "axios";
+import { useContext } from "react";
+import { AuthContext } from "../store/auth-context";
 
 function WelcomeScreen() {
+  const [fetchedMessage, setFetchedMessage] = useState("");
+
+  const authContext = useContext(AuthContext);
+  const token = authContext.token;
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://react-native-expensetrac-2b40c-default-rtdb.firebaseio.com/message.json?auth=" +
+          token
+      )
+      .then((response) => {
+        console.log(response.data);
+        setFetchedMessage(response.data);
+      });
+  }, [token]);
+
   return (
     <View style={styles.rootContainer}>
       <Text style={styles.title}>Welcome!</Text>
       <Text>You authenticated successfully!</Text>
+      <Text>{fetchedMessage}</Text>
     </View>
   );
 }
